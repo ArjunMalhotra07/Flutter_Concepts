c8f74d5bd2184ffca89dad7e93763304b99d361c
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2023-06-02 21:54:19 +0530

    Modified Modal, generated and registered modal adapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:audioplayers/audioplayers.dart';
+import 'package:extensions_demonstration/domain/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(TransactionAdapter());
   runApp(const MyApp());
 }
 
