e8fcf4a47dc641f20162618c8a0bff1572c2f379
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2023-06-03 23:56:03 +0530

    Implemented abstract classes

--- lib/main.dart
+++ lib/main.dart
@@ -27,39 +27,46 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  final ApiProviderImp apiProvider = ApiProviderImp();
+  @override
+  void initState() {
+    print(apiProvider.x);
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Value of X from Abstract class',
+              ),
+              Text(
+                '${apiProvider.x}',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+abstract class ApiProvider {
+  int x = 10;
+  Future fetchApiEndPoints();
+}
+
+class ApiProviderImp extends ApiProvider {
+  @override
+  Future fetchApiEndPoints() async {
+    print(x);
+    // TODO: implement fetchApiEndPoints
+    throw UnimplementedError();
   }
 }
